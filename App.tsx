db3b56c3929b311ecc052e8cd87d39735bb5a1bc
Mateusz Majgier <mateusz.majgier@likims.com>
2022-10-06 15:35:34 +0200

    add react navigation and dependencies

--- App.tsx
+++ App.tsx
@@ -1,13 +1,7 @@
 import React from "react";
 import { NativeBaseProvider, Box } from "native-base";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
-  return (
-    <NativeBaseProvider>
-      <Box bg={"red.100"} marginTop="100" rounded={10} paddingX="100">
-        Hello world
-      </Box>
-    </NativeBaseProvider>
-  );
+  return <NavigationContainer>{}</NavigationContainer>;
 }
